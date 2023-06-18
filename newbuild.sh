0ad5afe7a33b3aa7941d5ec5c97c89cd53bda3d1
George Zengin <george.zengin@gmail.com>
2023-06-19 02:24:53 +0300

    aaa

--- newbuild.sh
+++ newbuild.sh
@@ -2,10 +2,10 @@
 
 docker compose down
 docker ps -n 3 -q > /dev/null || docker rm $(docker ps -n 3 -q)
-docker rmi -f nginx-docker-compose-showip-app1:latest 
-docker rmi -f nginx-docker-compose-showip-app2:latest
-docker rmi -f nginx-docker-compose-showip-app3:latest
-docker volume rm nginx-docker-compose-showip_volgz 
+docker rmi -f ecs-ecr-docker-nginx-showip-app1:latest 
+docker rmi -f ecs-ecr-docker-nginx-showip-app2:latest
+docker rmi -f ecs-ecr-docker-nginx-showip-app3:latest
+docker volume rm ecs-ecr-docker-nginx-showip_volgz 
 
 if [[ $1 = "-r" ]]; then 
     docker-compose up --build
